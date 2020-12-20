02a03a454a7f82ec2e02e2de332a41b70a6e93fc
Anshad007 <4pa17cs006@pace.edu.in>
2020-12-20 11:36:52 +0530

    Made it persistant

--- js/index.js
+++ js/index.js
@@ -16,6 +16,9 @@ let taskList = document.getElementById("taskList");
 let addTaskBtn = document.getElementById("addTaskBtn");
 taskEnteringArea.value = "";
 
+//Variables to make data persistant
+let idForNewTask = 0;
+
 //Shift between timer types
 let switchToPomoBtn = document.getElementById("switchToPomoBtn");
 let switchToShortBtn = document.getElementById("switchToShortBtn");
@@ -68,32 +71,62 @@ function startTimer(evt) {
 timerControl.onclick = startTimer;
 
 // Task adding functionality*********************************************************
-addTaskBtn.onclick = function () {
-  let task = taskEnteringArea.value.trim();
 
-  if (task == "") {
-    return;
-  }
+function addTaskToList(task, id) {
   let taskTxtNode = document.createTextNode(task);
   let taskEle = document.createElement("li");
+  taskEle.id = id;
   let chkBtn = document.createElement("button");
   chkBtn.innerHTML = "Check";
 
-  chkBtn.onclick = function () {
+  function unCheckIt(e) {
+    taskEle.style.textDecoration = "none";
+    e.target.innerHTML = "Check";
+    e.target.onclick = checkIt;
+  }
+
+  function checkIt(e) {
     taskEle.style.textDecoration = "line-through";
-  };
+    e.target.innerHTML = "Uncheck";
+    e.target.onclick = unCheckIt;
+  }
+
+  chkBtn.onclick = checkIt;
 
   let delBtn = document.createElement("button");
   delBtn.innerHTML = "Delete";
 
   delBtn.onclick = function () {
+    localStorage.removeItem(taskEle.id);
     taskEle.remove();
   };
 
   taskEle.appendChild(chkBtn);
   taskEle.appendChild(taskTxtNode);
   taskEle.appendChild(delBtn);
-
   taskList.appendChild(taskEle);
+}
+
+addTaskBtn.onclick = function () {
+  let task = taskEnteringArea.value.trim();
+  if (task == "") {
+    return;
+  }
   taskEnteringArea.value = "";
+  addTaskToList(task, idForNewTask);
+  idForNewTask++;
+  localStorage.setItem("-1", idForNewTask);
+  localStorage.setItem(idForNewTask, task);
+};
+
+//When window loads *******************************************************************
+window.onload = function () {
+  let lSKeys = Object.keys(localStorage);
+  lSKeys.sort();
+  if (localStorage.getItem("-1")) {
+    idForNewTask = Number(localStorage.getItem(lSKeys.shift()));
+  }
+  lSKeys.forEach(function (key, i) {
+    addTaskToList(localStorage.getItem(key), key);
+  });
 };
